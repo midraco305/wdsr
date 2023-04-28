4aee8ee625acb4ff2bc468e4acd2cb5cf8ef800b
midraco305 <132033864+midraco305@users.noreply.github.com>
2023-04-28 10:29:57 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='wattpad_scraper',
-    version='0.1',
+    version='1.0.1',
     packages=find_packages(),
     install_requires=[
         'requests',
